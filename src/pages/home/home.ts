d0ac2f69877f4f2bceb8b3598fdf7f2661e4b72c
vithort <vithort@gmail.com>
2018-08-01 14:28:14 -0300

    Update

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,20 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { NavController, Slides } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  
+  @ViewChild(Slides) slides: Slides;
   constructor(public navCtrl: NavController) {
 
+  }
+
+  slideChanged() {
+    let currentIndex = this.slides.getActiveIndex();
+    console.log('Current index is', currentIndex);
   }
 
 }
